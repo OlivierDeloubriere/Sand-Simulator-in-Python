3730709911ead806a070669da91993ad33a35a4d
Olivier Deloubriere <olivier.deloubriere@gmail.com>
2024-04-01 12:22:38 +0200

    first commit

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pygame
 import world
 
-GRID_WIDTH = 2
+GRID_WIDTH = 4
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 400
-GRAIN_COLOR = (220, 245, 128)
+INITIAL_GRAIN_COLOR = (220, 245, 128)
 BACKGROUND_COLOR = (20,20,45)
-BUCKET_SIZE = 20
+BUCKET_SIZE = 10
+GRAIN_COLOR_PALETTE = [INITIAL_GRAIN_COLOR, (200, 225, 140), (220, 245, 180)]
 
 def updateWindow(grid, screen):
     screen.fill(BACKGROUND_COLOR)
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                pygame.draw.rect(screen, GRAIN_COLOR, pygame.Rect(GRID_WIDTH * i, GRID_WIDTH * j, GRID_WIDTH, GRID_WIDTH))
+            if grid[i][j] != (0,0,0):
+                pygame.draw.rect(screen, grid[i][j], pygame.Rect(GRID_WIDTH * i, GRID_WIDTH * j, GRID_WIDTH, GRID_WIDTH))
 
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     gameClock = pygame.time.Clock()
     gameIsRunning = True
-    
+    colorIndex = 0
+    changeColorCounter = 0
     gameWorld = world.World(WINDOW_HEIGHT // GRID_WIDTH, WINDOW_WIDTH // GRID_WIDTH)
     while gameIsRunning:
         for event in pygame.event.get():
@@ -31,7 +33,14 @@ if __name__ == "__main__":
         mouseButtonPressed = pygame.mouse.get_pressed()
         if any(mouseButtonPressed):
             mouseX, mouseY = pygame.mouse.get_pos()
-            gameWorld.addGrain(mouseX // GRID_WIDTH, mouseY // GRID_WIDTH, BUCKET_SIZE)
+            gameWorld.addGrain(mouseX // GRID_WIDTH, mouseY // GRID_WIDTH, BUCKET_SIZE, GRAIN_COLOR_PALETTE[colorIndex])
+            changeColorCounter += 1
+
+        if changeColorCounter == 20:
+            changeColorCounter = 0
+            colorIndex += 1
+            if colorIndex == len(GRAIN_COLOR_PALETTE):
+                colorIndex = 0
 
         gameWorld.update()
 
